71af9d7342c776c8d966a8996ba96924701921c9
Raghav0427 <84840084+Raghav0427@users.noreply.github.com>
2022-01-08 12:36:22 +0530

    Add files via upload

---
+++ MatrixAddition.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class MatrixAddition {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the Dimensions");
+        int rows=sc.nextInt();
+        int cols= sc.nextInt();
+        System.out.println("Enter Array a");
+        int a[][]=new int[rows][cols];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                a[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("Enter Array b");
+        int b[][]=new int[rows][cols];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                b[i][j]=sc.nextInt();
+            }
+        }
+        int c[][]=new int[rows][cols];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+               c[i][j]=a[i][j]+b[i][j];
+            }
+        }
+        System.out.println("The result is: ");
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                System.out.print(c[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
